8b761d502f370a4f97ead612a1f445c354a21859
davegregg <davegregg@gmail.com>
2021-09-27 09:21:13 -0400

    Starter code

---
+++ main.js
@@ -0,0 +1,22 @@
+/* The Advanced Counter object should have the following property and methods:
+
+  count: a number which represents the current count, initial value 0
+
+  render(): when this function runs, the DOM will be updated to display the current state of the counter
+
+  next(): increment the counter by 1, then render()
+
+  prev(): decrement the counter by 1, then render()
+
+  reset(): reset the counter to 0, then render()
+
+  skipForward(): increment the counter by 10, then render()
+
+  skipBackward(): increment the counter by 10, then   render()
+
+  startTimer(): start a timer and render() every second
+
+  stopTimer(): stop the timer, then render()
+*/
+
+const main = document.querySelector("main")
